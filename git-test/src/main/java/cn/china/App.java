79ac203f5e2888fbfea8d99867ee660d67e2eadf
zhousong <40654277@qq.com>
2016-08-29 19:44:20 +0800

    Initial commit

---
+++ git-test/src/main/java/cn/china/App.java
@@ -0,0 +1,19 @@
+package cn.china;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+
+        helloGit();
+    }
+
+    public static void helloGit(){
+        System.out.println("Hello Git.");
+    }
+}
+
